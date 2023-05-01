e7a99c595c4bcd15dba17ae0a6d1c2553d415a4b
DIDIVERG <90328061+DIDIVERG@users.noreply.github.com>
2023-05-01 20:23:20 +0300

    init

---
+++ Second/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SecondTask.Models
+{
+    public class BaseModel
+    {
+        
+    }
+}
---
+++ Second/Models/Season.cs
@@ -0,0 +1,7 @@
+﻿namespace SecondTask.Models
+{
+    public class Season
+    {
+        
+    }
+}
